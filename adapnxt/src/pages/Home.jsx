6dc1d096eba886115d7ead9bbb226e6270330444
harikesh2501 <135404019+harikesh2501@users.noreply.github.com>
2025-02-11 02:10:07 +0530

    Update Home.jsx

--- adapnxt/src/pages/Home.jsx
+++ adapnxt/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css';
-import LeftPart from '../components/Leftpart';
-import RightPart from '../components/Rightpart';
+import LeftPart from '../components/LeftPart';
+import RightPart from '../components/RightPart';
 
 
 function Home() {
